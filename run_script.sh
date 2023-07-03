481569c0145ef7a796550fb07f4e90533d2ebdd0
huma1994 <112937165+huma1994@users.noreply.github.com>
2023-07-03 12:21:19 +0545

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,1 +1,1 @@
-echo "hello world....from first git , jenkina with shell script22.. 777 "  > /root/projects/lab1_shell_script/abc
+echo "hello world....from first git , jenkina with shell script22.. 777 "  >  /var/lib/jenkins/workspace/abc
