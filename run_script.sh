e31a9879ca307e916311642fb41621cf088eef9d
huma1994 <112937165+huma1994@users.noreply.github.com>
2023-07-03 11:20:30 +0545

    Update run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,2 +1,1 @@
-echo "hello world....from first git , jenkina with shell script" > /tmp/file1
-echo "hello filr 2 from sanjeev thapa " > /tmp/file2
+echo "hello world....from first git , jenkina with shell script" 
